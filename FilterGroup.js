70d52bb304868bd373498e7fe430d30eabd1c60d
Mike Taylor <mike@indexdata.com>
2017-02-13 12:59:53 +0000

    Rewrite FilterGroup in functional form.

--- FilterGroup.js
+++ FilterGroup.js
@@ -2,17 +2,17 @@ import React, { PropTypes } from 'react'; // eslint-disable-line
 import FilterControlGroup from '@folio/stripes-components/lib/FilterControlGroup'; // eslint-disable-line
 import FilterCheckbox from './FilterCheckbox';
 
-class FilterGroup extends React.Component {
-  render() {
-    const { label, group, names, filters, onChangeFilter } = this.props;
+const FilterGroup = (props) => {
+  const { label, group, names, filters, onChangeFilter } = props;
 
-    return <FilterControlGroup label={label}>
-      {names.map((name, index) =>
-                 <FilterCheckbox key={index} group={group} name={name}
-                 onChangeFilter={onChangeFilter}
-                 checked={!!filters[`${group}.${name}`]} />)}
-    </FilterControlGroup>
-  }
-}
+  return (<FilterControlGroup label={label}>
+    {names.map((name, index) =>
+      <FilterCheckbox
+        key={index} group={group} name={name}
+        onChangeFilter={onChangeFilter}
+        checked={!!filters[`${group}.${name}`]}
+      />)}
+  </FilterControlGroup>);
+};
 
 export default FilterGroup;
