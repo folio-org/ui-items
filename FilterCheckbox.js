74408365949997b48265af2bafba1f06b2781392
Mike Taylor <mike@indexdata.com>
2017-02-13 12:48:57 +0000

    Rewrite FilterCheckbox in functional form.

--- FilterCheckbox.js
+++ FilterCheckbox.js
@@ -2,23 +2,21 @@ import React, { PropTypes } from 'react'; // eslint-disable-line
 import Checkbox from '@folio/stripes-components/lib/Checkbox'; // eslint-disable-line
 import Icon from '@folio/stripes-components/lib/Icon'; // eslint-disable-line
 
-class FilterCheckbox extends React.Component {
-  render() {
-    const { group, name, checked, onChangeFilter } = this.props;
-    const fullName = `${group}.${name}`;
+const FilterCheckbox = (props) => {
+  const { group, name, checked, onChangeFilter } = props;
+  const fullName = `${group}.${name}`;
 
-    return <Checkbox
-      id={`${fullName}-ItemFilter`}
-      label={name}
-      name={fullName}
-      checked={checked}
-      onChange={onChangeFilter}
-      marginBottom0
-      hover
-      fullWidth
-      checkedIcon={<Icon icon="eye" />}
-    />
-  }
-}
+  return (<Checkbox
+    id={`${fullName}-ItemFilter`}
+    label={name}
+    name={fullName}
+    checked={checked}
+    onChange={onChangeFilter}
+    marginBottom0
+    hover
+    fullWidth
+    checkedIcon={<Icon icon="eye" />}
+  />);
+};
 
 export default FilterCheckbox;
