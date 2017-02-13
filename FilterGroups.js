d90638f068d9b5a3a138b3dc8178deaf7c9da40f
Mike Taylor <mike@indexdata.com>
2017-02-13 17:42:51 +0000

    New file, FilterGroups, generates multiple filter groups.

---
+++ FilterGroups.js
@@ -0,0 +1,37 @@
+import React, { PropTypes } from 'react'; // eslint-disable-line
+import FilterGroup from './FilterGroup';
+
+const FilterGroups = (props) => {
+  const { config, filters, onChangeFilter } = props;
+
+  return (<span>
+    {config.map((group, index) =>
+      <FilterGroup
+        key={index}
+        label="{group.label}"
+        group="{group.group}"
+        names={group.fields.map(x => x[0])}
+        filters={filters} onChangeFilter={onChangeFilter}
+      />)
+    }
+  </span>);
+};
+
+FilterGroups.propTypes = {
+  config: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      group: PropTypes.string.isRequired,
+      fields: PropTypes.arrayOf(
+        PropTypes.arrayOf(
+          PropTypes.string.isRequired,
+          PropTypes.bool.isRequired,
+        ).isRequired,
+      ).isRequired,
+    }),
+  ).isRequired,
+  filters: PropTypes.object.isRequired,
+  onChangeFilter: PropTypes.func.isRequired,
+};
+
+export default FilterGroups;
