1ce826917ed163040ab7ed223d8530a0c9e7f311
Mike Taylor <mike@indexdata.com>
2017-02-14 19:10:19 +0000

    Move FilterCheckbox component into FilterGroups.js; get rid of FilterCheckbox.js

--- FilterGroups.js
+++ FilterGroups.js
@@ -1,6 +1,33 @@
 import React, { PropTypes } from 'react'; // eslint-disable-line
 import FilterControlGroup from '@folio/stripes-components/lib/FilterControlGroup'; // eslint-disable-line
-import FilterCheckbox from './FilterCheckbox';
+import Checkbox from '@folio/stripes-components/lib/Checkbox'; // eslint-disable-line
+import Icon from '@folio/stripes-components/lib/Icon'; // eslint-disable-line
+
+
+// private
+const FilterCheckbox = (props) => {
+  const { groupName, name, checked, onChangeFilter } = props;
+  const fullName = `${groupName}.${name}`;
+
+  return (<Checkbox
+    id={`${fullName}-ItemFilter`}
+    label={name}
+    name={fullName}
+    checked={checked}
+    onChange={onChangeFilter}
+    marginBottom0
+    hover
+    fullWidth
+    checkedIcon={<Icon icon="eye" />}
+  />);
+};
+
+FilterCheckbox.propTypes = {
+  groupName: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  checked: PropTypes.bool.isRequired,
+  onChangeFilter: PropTypes.func.isRequired,
+};
 
 
 // private
@@ -26,6 +53,7 @@ FilterGroup.propTypes = {
 };
 
 
+// public
 export function initialFilterState(config, filters) {
   const state = {};
 
