8e199571f03ea0a8f2b3708b5ffbe10eb34f41ab
Mike Taylor <mike@indexdata.com>
2017-02-14 19:01:25 +0000

    Move FilterGroup component into FilterGroups.js; get rid of FilterGroup.js

--- FilterGroups.js
+++ FilterGroups.js
@@ -1,5 +1,30 @@
 import React, { PropTypes } from 'react'; // eslint-disable-line
-import FilterGroup from './FilterGroup';
+import FilterControlGroup from '@folio/stripes-components/lib/FilterControlGroup'; // eslint-disable-line
+import FilterCheckbox from './FilterCheckbox';
+
+
+// private
+const FilterGroup = (props) => {
+  const { label, groupName, names, filters, onChangeFilter } = props;
+
+  return (<FilterControlGroup label={label}>
+    {names.map((name, index) =>
+      <FilterCheckbox
+        key={index} groupName={groupName} name={name}
+        onChangeFilter={onChangeFilter}
+        checked={!!filters[`${groupName}.${name}`]}
+      />)}
+  </FilterControlGroup>);
+};
+
+FilterGroup.propTypes = {
+  label: PropTypes.string.isRequired,
+  groupName: PropTypes.string.isRequired,
+  names: PropTypes.arrayOf(PropTypes.string).isRequired,
+  filters: PropTypes.object.isRequired,
+  onChangeFilter: PropTypes.func.isRequired,
+};
+
 
 export function initialFilterState(config, filters) {
   const state = {};
