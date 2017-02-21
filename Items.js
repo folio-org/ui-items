ed861f6709805894106243683546d130d338d458
Mike Taylor <mike@indexdata.com>
2017-02-21 10:19:56 -0500

    Move onChangeFilter function back into Items.js, on the basis of Jason's observation that the wrong abstraction is worse than no abstraction

--- Items.js
+++ Items.js
@@ -17,7 +17,7 @@ import Layer from '@folio/stripes-components/lib/Layer'; // eslint-disable-line
 import FilterPaneSearch from '@folio/stripes-components/lib/FilterPaneSearch'; // eslint-disable-line
 import FilterControlGroup from '@folio/stripes-components/lib/FilterControlGroup'; // eslint-disable-line
 
-import FilterGroups, { initialFilterState, filters2cql, onChangeFilter } from './FilterGroups';
+import FilterGroups, { initialFilterState, filters2cql } from './FilterGroups';
 
 import ViewItem from './ViewItem';
 
@@ -101,13 +101,21 @@ class Items extends React.Component {
     };
     props.mutator.addItemMode.replace({ mode: false });
 
-    this.onChangeFilter = onChangeFilter.bind(this);
+    this.onChangeFilter = this.onChangeFilter.bind(this);
     this.onChangeSearch = this.onChangeSearch.bind(this);
     this.onClearSearch = this.onClearSearch.bind(this);
     this.onSort = this.onSort.bind(this);
     this.onSelectRow = this.onSelectRow.bind(this);
 
     this.onClickAddNewItem = this.onClickAddNewItem.bind(this);
+  }
+
+  onChangeFilter(e) {
+    const filters = Object.assign({}, this.state.filters);
+    filters[e.target.name] = e.target.checked;
+    console.log('onChangeFilter setting state', filters);
+    this.setState({ filters });
+    this.updateSearch(this.state.searchTerm, this.state.sortOrder, filters);
   }
 
   onChangeSearch(e) {
