120a5b0e7e31eed8d6d27503e20cd4860917ea60
Matt Connolly <mjc12@cornell.edu>
2017-03-24 16:08:38 -0400

    Add material types list component
    This is an almost verbatim copy of the PatronGroupsList component in
    the ui-users module ... so it should probably be extracted out into
    a reusable component somewhere.

--- settings/MaterialTypesSettings.js
+++ settings/MaterialTypesSettings.js
@@ -4,6 +4,8 @@ import { connect } from '@folio/stripes-connect';
 import Paneset from '@folio/stripes-components/lib/Paneset';
 import Pane from '@folio/stripes-components/lib/Pane';
 
+import MaterialTypesList from './MaterialTypesList'
+
 class MaterialTypesSettings extends React.Component { 
   
   static manifest = Object.freeze({
@@ -54,7 +56,21 @@ class MaterialTypesSettings extends React.Component {
     return (
       <Paneset>
         <Pane defaultWidth="fill" >
-
+          <MaterialTypesList
+            // TODO: not sure why we need this OR if there are no groups
+            // Seems to load this once before the groups data from the manifest
+            // is pulled in. This still causes a JS warning, but not an error
+            contentData={this.props.data.materialTypes || []}
+            label="Material Types"
+            createButtonLabel="+ Add type"
+            visibleFields={['name']}
+            itemTemplate={{name:'string', inUse:'bool'}}
+            actionSuppression={suppressor}
+            onUpdate={this.onUpdateType}
+            onCreate={this.onCreateType}
+            onDelete={this.onDeleteType}
+            isEmptyMessage="There are no Material Types"
+          />
         </Pane>
       </Paneset>
     );
