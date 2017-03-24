3f8ecd923b98cd78a9b7f590c32867bad4df8a2a
Matt Connolly <mjc12@cornell.edu>
2017-03-24 15:54:05 -0400

    Add skeleton CRUD functions

--- settings/MaterialTypesSettings.js
+++ settings/MaterialTypesSettings.js
@@ -6,14 +6,58 @@ import Pane from '@folio/stripes-components/lib/Pane';
 
 class MaterialTypesSettings extends React.Component { 
   
+  static manifest = Object.freeze({
+    materialTypes: {
+      type: 'okapi',
+      path: 'material-type',
+      records: 'mtypes',
+      PUT: {
+        path: 'material-type/${activeRecord.id}',
+      },
+      DELETE: {
+        path: 'material-type/${activeRecord.id}'
+      }
+    },
+    activeRecord: {}
+  });
+
   constructor(props){
     super(props);
+    
+    this.state = {}
+    
+    this.onCreateType = this.onCreateType.bind(this);
+    this.onUpdateType = this.onUpdateType.bind(this);
+    this.onDeleteType = this.onDeleteType.bind(this);
+  }
+  
+  onCreateType(type) {
+    
+  }
+  
+  onUpdateType(type) {
+    
+  }
+  
+  onDeleteType(type) {
+    
   }
   
   render() {
+    
+    const suppressor = {
+      // If a suppressor returns true, the control for that action will not appear
+      delete: (type) => { return (!type.inUse) }, 
+      edit: type => false,                      
+    }
+    
     return (
-      <p>Boo!</p>
-    )
+      <Paneset>
+        <Pane defaultWidth="fill" >
+
+        </Pane>
+      </Paneset>
+    );
   }
   
 }
