74f8dab0a1a6e887e5b37844b9ccdbeccf49b91b
Matt Connolly <mjc12@cornell.edu>
2017-05-03 15:14:27 -0400

    Deal with linting errors

--- settings/LoanTypesSettings.js
+++ settings/LoanTypesSettings.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Component } from 'react';
-import { connect } from '@folio/stripes-connect';
 import Paneset from '@folio/stripes-components/lib/Paneset';
 import Pane from '@folio/stripes-components/lib/Pane';
 import EditableList from '@folio/stripes-components/lib/structures/EditableList';
 
-class LoanTypesSettings extends React.Component { 
+class LoanTypesSettings extends React.Component {
+
+  static propTypes = {
+    data: PropTypes.object.isRequired,
+    mutator: PropTypes.shape({
+      activeRecord: PropTypes.shape({
+        update: PropTypes.func,
+      }),
+      loanTypes: PropTypes.shape({
+        POST: PropTypes.func,
+        PUT: PropTypes.func,
+        DELETE: PropTypes.func,
+      }),
+    }).isRequired,
+    paneWidth: PropTypes.string.isRequired,
+  };
 
   static manifest = Object.freeze({
     loanTypes: {
@@ -16,47 +30,46 @@ class LoanTypesSettings extends React.Component {
         path: 'loan-types/${activeRecord.id}',
       },
       DELETE: {
-        path: 'loan-types/${activeRecord.id}'
-      }
+        path: 'loan-types/${activeRecord.id}',
+      },
     },
-    activeRecord: {}
+    activeRecord: {},
   });
-  
-  constructor(props){
+
+  constructor(props) {
     super(props);
-    
-    this.state = {}
-    
+
+    this.state = {};
+
     this.onCreateType = this.onCreateType.bind(this);
     this.onUpdateType = this.onUpdateType.bind(this);
     this.onDeleteType = this.onDeleteType.bind(this);
   }
-  
+
   onCreateType(type) {
-    console.log('ui-items - settings - onCreateType called')
+    console.log('ui-items - settings - onCreateType called');
     this.props.mutator.loanTypes.POST(type);
   }
-  
+
   onUpdateType(type) {
-    console.log('ui-items - settings - onUpdateType called')
-    this.props.mutator.activeRecord.update({'id': type.id });
+    console.log('ui-items - settings - onUpdateType called');
+    this.props.mutator.activeRecord.update({ id: type.id });
     this.props.mutator.loanTypes.PUT(type);
   }
-  
+
   onDeleteType(typeId) {
-    console.log('ui-items - settings - onDeleteType called')
-    this.props.mutator.activeRecord.update({'id': typeId });
-    this.props.mutator.loanTypes.DELETE(this.props.data.loanTypes.find((t) => { return t.id == typeId }))
+    console.log('ui-items - settings - onDeleteType called');
+    this.props.mutator.activeRecord.update({ id: typeId });
+    this.props.mutator.loanTypes.DELETE(this.props.data.loanTypes.find((t) => { return t.id === typeId; }));
   }
-  
+
   render() {
-    
     const suppressor = {
       // If a suppressor returns true, the control for that action will not appear
-      delete: type => true, 
-      edit: type => false,                      
-    }
-    
+      delete: () => true,
+      edit: () => false,
+    };
+
     return (
       <Paneset>
         <Pane defaultWidth="fill" >
@@ -68,7 +81,7 @@ class LoanTypesSettings extends React.Component {
             label="Loan Types"
             createButtonLabel="+ Add type"
             visibleFields={['name']}
-            itemTemplate={{name:'string'}}
+            itemTemplate={{ name: 'string' }}
             actionSuppression={suppressor}
             onUpdate={this.onUpdateType}
             onCreate={this.onCreateType}
@@ -79,7 +92,7 @@ class LoanTypesSettings extends React.Component {
       </Paneset>
     );
   }
-  
+
 }
 
-export default connect(LoanTypesSettings, '@folio/ui-items');
+export default LoanTypesSettings;
