808a95e6ca38760e835e3638cb1b76fa7d1baf5f
Matt Connolly <mjc12@cornell.edu>
2017-05-03 15:33:18 -0400

    Point material type settings to right endpoint; crush linting errors

--- settings/MaterialTypesSettings.js
+++ settings/MaterialTypesSettings.js
@@ -1,62 +1,74 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { PropTypes } from 'react';
 import Paneset from '@folio/stripes-components/lib/Paneset';
 import Pane from '@folio/stripes-components/lib/Pane';
 import EditableList from '@folio/stripes-components/lib/structures/EditableList';
 
-class MaterialTypesSettings extends React.Component { 
-  
+class MaterialTypesSettings extends React.Component {
+
+  static propTypes = {
+    data: PropTypes.object.isRequired,
+    mutator: PropTypes.shape({
+      activeRecord: PropTypes.shape({
+        update: PropTypes.func,
+      }),
+      materialTypes: PropTypes.shape({
+        POST: PropTypes.func,
+        PUT: PropTypes.func,
+        DELETE: PropTypes.func,
+      }),
+    }).isRequired,
+  };
+
   static manifest = Object.freeze({
     materialTypes: {
       type: 'okapi',
-      path: 'material-type',
+      path: 'material-types',
       records: 'mtypes',
       PUT: {
-        path: 'material-type/${activeRecord.id}',
+        path: 'material-types/${activeRecord.id}',
       },
       DELETE: {
-        path: 'material-type/${activeRecord.id}'
-      }
+        path: 'material-types/${activeRecord.id}',
+      },
     },
-    activeRecord: {}
+    activeRecord: {},
   });
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    
-    this.state = {}
-    
+
+    this.state = {};
+
     this.onCreateType = this.onCreateType.bind(this);
     this.onUpdateType = this.onUpdateType.bind(this);
     this.onDeleteType = this.onDeleteType.bind(this);
   }
-  
+
   onCreateType(type) {
-    console.log('ui-items - settings - onCreateType called')
+    console.log('ui-items - settings - onCreateType called');
     this.props.mutator.materialTypes.POST(type);
   }
-  
+
   onUpdateType(type) {
-    console.log('ui-items - settings - onUpdateType called')
-    this.props.mutator.activeRecord.update({'id': type.id });
+    console.log('ui-items - settings - onUpdateType called');
+    this.props.mutator.activeRecord.update({ id: type.id });
     this.props.mutator.materialTypes.PUT(type);
   }
-  
+
   onDeleteType(typeId) {
-    console.log('ui-items - settings - onDeleteType called')
-    this.props.mutator.activeRecord.update({'id': typeId });
-    this.props.mutator.materialTypes.DELETE(this.props.data.materialTypes.find((t) => { return t.id == typeId }))
+    console.log('ui-items - settings - onDeleteType called');
+    this.props.mutator.activeRecord.update({ id: typeId });
+    this.props.mutator.materialTypes.DELETE(this.props.data.materialTypes.find(t => t.id === typeId));
   }
-  
+
   render() {
-    
     const suppressor = {
       // If a suppressor returns true, the control for that action will not appear
-    //delete: (type) => { return (!type.inUse) }, 
-      delete: type => true,
-      edit: type => false,                      
-    }
-    
+    // delete: (type) => { return (!type.inUse) },
+      delete: () => true,
+      edit: () => false,
+    };
+
     return (
       <Paneset>
         <Pane defaultWidth="fill" fluidContentWidth>
@@ -68,7 +80,7 @@ class MaterialTypesSettings extends React.Component {
             label="Material Types"
             createButtonLabel="+ Add type"
             visibleFields={['name']}
-            itemTemplate={{name:'string'}}
+            itemTemplate={{ name: 'string' }}
             actionSuppression={suppressor}
             onUpdate={this.onUpdateType}
             onCreate={this.onCreateType}
@@ -79,7 +91,7 @@ class MaterialTypesSettings extends React.Component {
       </Paneset>
     );
   }
-  
+
 }
 
 export default MaterialTypesSettings;
