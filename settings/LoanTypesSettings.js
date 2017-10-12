754cb95d4a00345959b1af3fc39e3965ffd56df0
Zak Burke <zburke@cornell.edu>
2017-10-12 16:32:27 -0400

    Import PropTypes from prop-types, not React. Refs STRIPES-427

--- settings/LoanTypesSettings.js
+++ settings/LoanTypesSettings.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ControlledVocab from '@folio/stripes-smart-components/lib/ControlledVocab';
 
 class LoanTypesSettings extends React.Component {
